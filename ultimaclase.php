03485bf5659a320d91e437dd384a66d7201045a7
dannychavez141 <dannychavez141@users.noreply.github.com>
2019-05-14 22:00:42 -0500

    fin creo XD

---
+++ ultimaclase.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+ include_once'./conexion.php'; 
+ $bus = $_GET['bus'];
+$sql="select * from asistencia.clase order by idClase desc limit 1;";
+
+        $rs=$mysqli->query($sql);         
+   while ($row = $rs ->fetch_array()) {
+
+                  $alumno[]=array_map('utf8_encode',$row);
+
+
+}  echo json_encode($alumno);
+        
+        $rs -> close();
+    
+
+
+?>
