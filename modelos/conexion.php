3f4b4fa9957d76076413e3d7a674dbbf784e8aa3
danny chavez <dannychavez141@users.noreply.github.com>
2019-11-10 20:04:44 -0500

    g

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -1,27 +1,14 @@
-
 <?php
-<<<<<<< HEAD
 //$mysqli = new mysqli("localhost", "root", "", "asistencia");
+//$mysqli = new mysqli("localhost", "root", "", "asistencia");
+//$mysqli = new mysqli("sql307.tonohost.com","ottos_24465265","Teamarex100pre","ottos_24465265_asistencia"); 
+//$mysqli = new mysqli("localhost","id11530952_proyectounu2019","losloles54","id11530952_asistencia"); 
+//$mysqli = new mysqli("35.238.55.2","unu2019","losloles54","asistencia"); 
 $mysqli = new mysqli("sql202.epizy.com","epiz_24776534","lQIE6dbtPgq","epiz_24776534_asistencia"); 
+
 if ($mysqli->connect_errno) {
     echo 'error al conectar';
     echo 'error' . $mysqli->connect_errno;
-}else
-{
-  
 }
-=======
-     //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-	//$mysqli = new mysqli("sql307.tonohost.com","ottos_24465265","Teamarex100pre","ottos_24465265_asistencia"); 
-//$mysqli = new mysqli("localhost","root","","asistencia"); 
-//$mysqli = new mysqli("localhost","id11530952_proyectounu2019","losloles54","id11530952_asistencia"); 
-$mysqli = new mysqli("35.238.55.2","unu2019","losloles54","asistencia"); 
-	if(mysqli_connect_errno()){
-		echo 'Conexion Fallida : ', mysqli_connect_error();
-		exit();
-	}
-
-
->>>>>>> 7b8f917042c8e9cd2195be0293eff088885af18a
 
 ?>
--- vdocentes.php
+++ vdocentes.php
@@ -7,15 +7,10 @@ $sql="SELECT * FROM docente;";
 
                   $curso[]=array_map('utf8_encode',$row);
 
-<<<<<<< HEAD
-} header('Content-type=application/json; charset=utf-8');
- echo json_encode($curso);
-=======
 }  
 $lista=json_encode($curso,JSON_UNESCAPED_UNICODE);
 header('Content-Type: application/json; charset=utf8');
-echo $lista;
->>>>>>> 7b8f917042c8e9cd2195be0293eff088885af18a
+
         
         $rs -> close();
     
