9c717b69b279e01dae16fee51c778c9428239610
danny chavez <dannychavez141@users.noreply.github.com>
2019-11-10 19:53:50 -0500

    g

--- vdocentes.php
+++ vdocentes.php
@@ -1,5 +1,4 @@
 <?php
-error_reporting(0);
  include_once'./conexion.php'; 
 $sql="SELECT * FROM docente;";
 
@@ -8,8 +7,15 @@ $sql="SELECT * FROM docente;";
 
                   $curso[]=array_map('utf8_encode',$row);
 
+<<<<<<< HEAD
 } header('Content-type=application/json; charset=utf-8');
  echo json_encode($curso);
+=======
+}  
+$lista=json_encode($curso,JSON_UNESCAPED_UNICODE);
+header('Content-Type: application/json; charset=utf8');
+echo $lista;
+>>>>>>> 7b8f917042c8e9cd2195be0293eff088885af18a
         
         $rs -> close();
     
