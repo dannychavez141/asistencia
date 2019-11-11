ee1f0c65b9d2b51e8cdd5a0fae1c13cf046a502f
danny chavez <dannychavez141@users.noreply.github.com>
2019-11-10 22:59:59 -0500

    g

--- control/conexion.php
+++ control/conexion.php
@@ -1,10 +1,9 @@
 <?php
-//$mysqli = new mysqli("localhost", "root", "", "asistencia");
-//$mysqli = new mysqli("localhost", "root", "", "asistencia");
+$mysqli = new mysqli("192.168.1.35", "root", "", "asistencia");
 //$mysqli = new mysqli("sql307.tonohost.com","ottos_24465265","Teamarex100pre","ottos_24465265_asistencia"); 
 //$mysqli = new mysqli("localhost","id11530952_proyectounu2019","losloles54","id11530952_asistencia"); 
 //$mysqli = new mysqli("35.238.55.2","unu2019","losloles54","asistencia"); 
-$mysqli = new mysqli("sql202.epizy.com","epiz_24776534","lQIE6dbtPgq","epiz_24776534_asistencia"); 
+//$mysqli = new mysqli("sql202.epizy.com","epiz_24776534","lQIE6dbtPgq","epiz_24776534_asistencia"); 
 
 if ($mysqli->connect_errno) {
     echo 'error al conectar:';
--- vdocentes.php
+++ vdocentes.php
@@ -1,19 +1,20 @@
 <?php
- include_once'./conexion.php'; 
+ob_start();
+ require'./conexion.php'; 
 $sql="SELECT * FROM docente;";
-
-        $rs=$mysqli->query($sql);         
-   while ($row = $rs ->fetch_array()) {
-
-                  $curso[]=array_map('utf8_encode',$row);
-
-}  
-$lista=json_encode($curso,JSON_UNESCAPED_UNICODE);
-header('Content-Type: application/json; charset=utf8');
-
+$rs=$mysqli->query($sql);         
+ while ($row = $rs ->fetch_array()) {
+$curso[]=array_map('utf8_encode',$row);
+}   
+$rs -> close();
+$mysqli->close();
+header("Content-Type: application/json; charset=UTF-8");
+ob_end_clean();
+echo utf8_encode(json_encode($curso));
         
-        $rs -> close();
+       
     
 
 
 ?>
+
