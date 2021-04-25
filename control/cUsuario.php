e325c6f6604598357b74640b45eb6df6547dd1de
danny chavez <dannychavez141@users.noreply.github.com>
2021-04-25 07:22:34 -0500

    g

--- control/cUsuario.php
+++ control/cUsuario.php
@@ -12,12 +12,41 @@
  * @author DaHex2
  */
 class cUsuario {
-    function login($usuario,$clave) {
-         $conexion = new cConexion();
+
+    function login($usuario, $clave) {
+        $conexion = new cConexion();
         $sql = "SELECT * FROM docente where dni='$usuario' and  pass='$clave'";
         $bd = $conexion->getBd();
         $rs = $bd->query($sql);
-        $datos=$rs->fetch_array();
+        $datos = $rs->fetch_array();
         return $datos;
     }
+
+    function crearToken($datos) {
+        $jmt = new \Firebase\JWT\JWT();
+        $tiempo = 1; // Horas
+        $time = time();
+        $encrypt = array('HS256');
+        $token = array(
+            'exp' => $time + (3600 * $tiempo),
+            'aud' => array(
+                'id' => "{$datos['idusuario']}"
+            ),
+            'data' => array(
+                'nombres' => "{$datos['nombres']}",
+                'apellidos' => "{$datos['apellidos']}",
+                'usuario' => "{$datos['user']}"
+            )
+        );
+        $t = $jmt->encode($token, "1234");
+        return $t;
+    }
+
+    function validar($token) {
+        $jmt = new \Firebase\JWT\JWT();
+        $encrypt = array('HS256');
+        $e = $jmt->decode($token, "1234", $encrypt);
+        return $e;
+    }
+
 }
