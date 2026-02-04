8a3fedb95cd72e868b4b10b532c08b4413bc253e
Danny Manuel Chávez Herrera <dannychavez141@gmail.com>
2026-02-03 22:19:12 -0500

    Update vActividadDoc.dart

--- asistenciaApp/lib/viewDocente/vActividadDoc.dart
+++ asistenciaApp/lib/viewDocente/vActividadDoc.dart
@@ -179,7 +179,7 @@ class _vActividadDocState extends State<vActividadDoc> {
                   )
                 ],
               )),
-        /*  Container(
+        / Container(
               margin: const EdgeInsets.all(4),
               width: MediaQuery.of(context).size.width * 0.20,
               child: Column(children: [
@@ -203,7 +203,7 @@ class _vActividadDocState extends State<vActividadDoc> {
                           ..strokeWidth = 1
                           ..color = Colors.red[700]!,
                       )))
-              ]))*/
+              ]))
         ],
       )));
     }
