7666b5b542929f4ed6ec109a357e38e429edfcae
RickyGatu <50969477+RickyGatu@users.noreply.github.com>
2026-08-19 12:59:21 -0500

    ultimos cambios

--- asistenciaApp/lib/pdf/saveWeb.dart
+++ asistenciaApp/lib/pdf/saveWeb.dart
@@ -2,13 +2,13 @@
 import 'dart:async';
 import 'dart:convert';
 // ignore: avoid_web_libraries_in_flutter
-import 'dart:html';
+//import 'dart:html';
 
 ///To save the pdf file in the device
 Future<void> saveAndLaunchFileweb(List<int> bytes, String fileName) async {
-  AnchorElement(
+  /*AnchorElement(
       href:
       'data:application/octet-stream;charset=utf-16le;base64,${base64.encode(bytes)}')
     ..setAttribute('download', fileName)
-    ..click();
+    ..click();*/
 }
